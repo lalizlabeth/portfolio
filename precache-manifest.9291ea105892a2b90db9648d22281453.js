a5f4c8c4e0f0cacf376bb34beb06dcbed874f908
lalizlabeth <elizabethylin@gmail.com>
2019-03-20 09:26:01 -0400

    Updates

---
+++ precache-manifest.9291ea105892a2b90db9648d22281453.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b27071aaff80f9897155b155bf660e87",
+    "url": "/portfolio/static/media/thumb-student-progress.b27071aa.png"
+  },
+  {
+    "revision": "8cb5b1be127fdb398afcad5fd6a36355",
+    "url": "/portfolio/static/media/old-1.8cb5b1be.png"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "474c03bbc92958160cc1",
+    "url": "/portfolio/static/js/main.9e7503d6.chunk.js"
+  },
+  {
+    "revision": "a202b491752ff940e466",
+    "url": "/portfolio/static/js/2.e59543cb.chunk.js"
+  },
+  {
+    "revision": "474c03bbc92958160cc1",
+    "url": "/portfolio/static/css/main.6ad06391.chunk.css"
+  },
+  {
+    "revision": "a202b491752ff940e466",
+    "url": "/portfolio/static/css/2.a80c3312.chunk.css"
+  },
+  {
+    "revision": "bee1ab0023f120e0a29fc4d89f40a1e0",
+    "url": "/portfolio/index.html"
+  }
+];
