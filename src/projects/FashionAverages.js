0ee1017058592b2d25d31dbfb3be24637def590c
lalizlabeth <elizabethylin@gmail.com>
2019-04-01 10:04:50 -0700

    Fashion averages

--- src/projects/FashionAverages.js
+++ src/projects/FashionAverages.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class StudentProgress extends Component {
+class FashionAverages extends Component {
   render() {
     return (
       <section className="col-7 nested">
@@ -10,7 +10,7 @@ class StudentProgress extends Component {
 
         <div className="text-group">
           <p className="large-paragraph medium-spacer-bottom col-5">We learned about Jason Salavon's image averages in one of the classes. I wanted to apply this methodology to fashion runway images I had scraped from Vogue.com using Python. This is the average of every runway model in their Spring 2019 database generated with Open Frameworks:</p>
-          <p className="reflections col-2">My notes and reflections about the project will go here!</p>
+          <p className="reflections col-2">This was my fav project at sfpc because it was magical <span role="img" aria-label="sparkle">✨</span> to watch the image get generated.</p>
         </div>
 
         <div className="col-5 medium-spacer-bottom">
@@ -39,10 +39,37 @@ class StudentProgress extends Component {
           <img className="project-image" alt="Telfar" src={require("../assets/images/fashion-avgs/chromat.jpg")}/>
         </div>
 
+        <div className="text-group">
+          <p className="large-paragraph medium-spacer-bottom col-5">Comme des Garçons by Rei Kawakubo averages light skinned, but her averages create an interesting effect because they're all so different and avant garde.</p>
+          <p className="reflections col-2">My notes and reflections about the project will go here!</p>
+        </div>
+
+        <div className="col-5 medium-spacer-bottom">
+          <img className="project-image" alt="Comme des Garcons" src={require("../assets/images/fashion-avgs/cdg.jpg")}/>
+        </div>
+
+        <div className="text-group">
+          <p className="large-paragraph medium-spacer-bottom col-5">Most European brands perpetuate the idealized model body and also tend to be less innovative from season to season.</p>
+          <p className="reflections col-2">My notes and reflections about the project will go here!</p>
+        </div>
+
+        <div className="col-5 medium-spacer-bottom">
+          <img className="project-image" alt="Dries van Noten" src={require("../assets/images/fashion-avgs/dries.jpg")}/>
+        </div>
+
+        <div className="col-5 medium-spacer-bottom">
+          <img className="project-image" alt="Dries van Noten" src={require("../assets/images/fashion-avgs/mj.jpg")}/>
+        </div>
+
+        <div className="col-5 medium-spacer-bottom">
+          <img className="project-image" alt="Dries van Noten" src={require("../assets/images/fashion-avgs/louis.jpg")}/>
+        </div>
+
+
 
     </section>
     );
   }
 }
 
-export default StudentProgress;
+export default FashionAverages;
